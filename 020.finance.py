457f1d066bd46830d7bfe9a69bcf55f9baad446a
runnablek <runnablek@gmail.com>
2015-05-17 01:54:36 +0900

    company 콜렉션에 이는 모든 기업의 코드를 가져와서 재무정보 데이터를 긁어옴,requests 를 urllib2 로 대체함,기능별로 function 을 나눔

--- 020.finance.py
+++ 020.finance.py
@@ -1,12 +1,22 @@
 #-*- coding: utf-8 -*-
-import requests
 import json
+import urllib2
 from pymongo import MongoClient
 try:
     from BeautifulSoup import BeautifulSoup 
 except ImportError:
     from bs4 import BeautifulSoup              # exception  import 
 
+
+gnUrl = 'http://companyinfo.stock.naver.com/v1/company/cF1001.aspx?finGubun=MAIN&cmp_cd='
+gnOpener = urllib2.build_opener()
+gnOpener.addheaders = [('User-agent', 'Mozilla/5.0')]                 # header define
+
+
+_client = MongoClient()
+_db = _client.hpm
+gnCompanyColl = _db.company
+gnFinanceColl = _db.finance
 
 def get_days_to_json(soup):
     script = soup.findAll('script')[4].string
@@ -24,75 +34,98 @@ def get_data_to_json(soup):
     data = json.loads(data)    
     return data
     #print script    
-    
-code = '014530'
 
-
-# page2
-url = 'http://companyinfo.stock.naver.com/v1/company/cF1001.aspx?finGubun=MAIN&cmp_cd=' + code
-r = requests.get(url)
-soup = BeautifulSoup(r.text)
-
-
-days = get_days_to_json(soup)
-data = get_data_to_json(soup)
-
-              
-
-# embedded document 구조로 저장하기 
-finance_list = [];
-finance  = {};
-finance["code"] = code
-finance_list.append(finance);
-
-#print finan_list
-year_data_list = [];
-quater_data_list = [];
-
-
-
-year_day = days[0]
-quater_day = days[1]
-
-for data1 in data:
-    
-    yy_dat = data1[0]    
-    qt_dat = data1[1]    
-
-    jj = 0
-    for yy_dat1 in yy_dat:
+def set_year_and_quater(days, data, year_data_list, quater_data_list) :
         
-        dnam = yy_dat1[0]
+    year_day = days[0]
+    quater_day = days[1]
+    
+    for data1 in data:
         
-        qt_dat1 = qt_dat[jj]
-        jj = jj + 1
-
-        ii = 0            
-        for yy_dat2 in yy_dat1[1:]:
-            #print len(qt_dat1[ii])
-            qt_dat2 = qt_dat1[ii]                
+        yy_dat = data1[0]    
+        qt_dat = data1[1]    
+    
+        jj = 0
+        for yy_dat1 in yy_dat:
             
-            year_data = {}            
-            year_data["day"] = year_day[ii]
-            year_data["item_name"] = dnam
-            year_data["item_value"] = yy_dat2.replace(',', '')
-            year_data_list.append(year_data);
+            dnam = yy_dat1[0]
+            
+            qt_dat1 = qt_dat[jj]
+            jj = jj + 1
+    
+            ii = 0            
+            for yy_dat2 in yy_dat1[1:]:
+                #print len(qt_dat1[ii])
+                qt_dat2 = qt_dat1[ii]                
                 
-            quater_data = {}            
-            quater_data["day"] = quater_day[ii]
-            quater_data["item_name"] = dnam
-            quater_data["item_value"] = qt_dat2.replace(',', '')
-            quater_data_list.append(quater_data);
-            
-            ii = ii + 1;
-            #print iid
+                year_data = {}            
+                year_data["day"] = year_day[ii]
+                year_data["item_name"] = dnam
+                year_data["item_value"] = yy_dat2.replace(',', '')
+                year_data_list.append(year_data);
+                    
+                quater_data = {}            
+                quater_data["day"] = quater_day[ii]
+                quater_data["item_name"] = dnam
+                quater_data["item_value"] = qt_dat2.replace(',', '')
+                
+                #print quater_data
+                quater_data_list.append(quater_data);
+                
+                ii = ii + 1;
+                #print iid
+    
 
- 
-client = MongoClient()
-db = client.hpm
-coll = db.finance
+def insert_finance(ncode):
+    nUrl = gnUrl + ncode    
+    nResponse = gnOpener.open(nUrl)
+    nPage = nResponse.read()
+    # page2
+    # r = requests.get(gnUrl)
+    nSoup = BeautifulSoup(nPage)
 
-coll.insert(finance_list)
-coll.update({"code": code}, { "$push": {"year_date" : year_data_list, "quater_data" : quater_data_list } })
+    days = get_days_to_json(nSoup)
+    data = get_data_to_json(nSoup)
+
+    #print days
+    # embedded document 구조하기 위해서 root 에 code 저장
+    finance_list = [];
+    finance  = {};
+    finance["code"] = ncode
+    finance_list.append(finance);
+    
+    #print finance_list
+    gnFinanceColl.insert(finance_list)
+
+    # year, quater  1차 json 데이터를 finance 구조에 맞게 다시 저장함 
+    year_data_list = [];
+    quater_data_list = [];
+   
+    
+    set_year_and_quater(days, data, year_data_list, quater_data_list)
+    gnFinanceColl.update({"code": ncode}, { "$push": {"year" : year_data_list, "quater" : quater_data_list } })
+    
+    print ncode
+     
+     
+def load_company_all():
+    _companys = gnCompanyColl.find();
+    return _companys;
+    
+
+#############3 main ################################
+
+#insert_finance('192400')
+
+nCompanys = load_company_all()
+for company in nCompanys[:]
+    insert_finance(company['code'][1:])
+    
+    
+
+
+
+
+
 
 
